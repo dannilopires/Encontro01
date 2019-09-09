282310333709501e92f4f31fefe8475743631ddd
dannilo <dannilo.zorn@gmail.com>
2019-09-09 16:13:57 -0300

    4 e 5

---
+++ Encontro03/questao4.js
@@ -0,0 +1,12 @@
+function nome() {
+    let nome = document.getElementById("demo").value;
+    alert("O nome completo é: " + nome);
+}
+
+function qtdechar(){
+    let nome = document.getElementById("demo").value;
+    let qtd = nome.length; 
+    alert("A quantidade de caracteres é igual a " + qtd);
+
+    
+}
